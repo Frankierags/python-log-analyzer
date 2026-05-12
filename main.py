a824d1f876d6cb65e79632df54d3e6e2d01f1735
frankierags <franktrags@gmail.com>
2026-05-12 02:18:16 -0500

    Added IP tracking and time-based suspicious login detection

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
+from datetime import datetime
+import re
 with open("sample_logs.txt", "r") as file:
+    failedCount = 0  
+    linecount = 0
+    failedIP = {
+
+    }
     for line in file:
-        print(line.strip())
+        linecount+=1
+        line = line.strip()
+        match = re.search(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}", line)
+        ip = re.search(r"\d+\.\d+\.\d+\.\d+", line)
+
+        if(linecount>1):
+            oldTime = newTime
+            newTime = datetime.strptime(match.group(), "%Y-%m-%d %H:%M:%S")
+            timeDifference = newTime - oldTime
+            if(timeDifference.total_seconds()>= 10):
+                failedCount = 0
+        else:
+            newTime = datetime.strptime(match.group(), "%Y-%m-%d %H:%M:%S")
+        
+
+        words = line.split()
+        for word in words:
+            if(word == "LOGIN_FAILED"):
+                failedCount += 1
+                print("LOGIN FAIL")
+                print("Current Count", failedCount)
+                if ip.group() not in failedIP:
+                    failedIP[ip.group()] = 1
+                else:
+                    failedIP[ip.group()] += 1
+                    print(failedIP[ip.group()])
+
+                if (failedCount > 2):
+                    print("Suspicous Activity Detected")
+
+
+                
+            
+
+
